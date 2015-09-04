cf2016a46b736f94c3e62ded3bc9a130e47882ad
141220028-Fang Kang <1010098686@qq.com>
2015-09-04 15:09:09 +0800

    finish the definition of structure CPU_state

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -25,9 +25,10 @@ typedef struct {
 	  }gpr[8];
 
 	/* Do NOT change the order of the GPRs' definitions. */
-    struct {
-	uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-	};
+      struct 
+	   {
+	     uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+	   };
 	};
 	swaddr_t eip;
 
