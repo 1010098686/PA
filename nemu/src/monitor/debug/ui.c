81907242fe10b05f2e7d80ab693023d35aa1dd11
tracer-ics2015 <tracer@njuics.org>
2015-09-05 05:46:27 +0800

    >  compile NEMU 141220028 fangkang Linux fk 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  05:46:27 up  4:45,  2 users,  load average: 0.08, 0.04, 0.05 cc8de8ebfc46d8c05f002b10de2b238a6d8e77fe

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -95,14 +95,14 @@ static int cmd_info(char* args)
 {
 	if (strcmp(args,"r") == 0)
 	{
-		printf("eax:%d\n",cpu.eax);
-		printf("edx:%d\n",cpu.edx);
-		printf("ecx:%d\n",cpu.ecx);
-		printf("ebx:%d\n",cpu.ebx);
-		printf("ebp:%d\n",cpu.ebp);
-		printf("esi:%d\n",cpu.esi);
-		printf("edi:%d\n",cpu.edi);
-		printf("esp:%d\n",cpu.esp);
+		printf("eax:	0x%x	%d\n",cpu.eax,cpu.eax);
+		printf("edx:	0x%x	%d\n",cpu.edx,cpu.edx);
+		printf("ecx:	0x%x	%d\n",cpu.ecx,cpu.ecx);
+		printf("ebx:	0x%x	%d\n",cpu.ebx,cpu.ebx);
+		printf("ebp:	0x%x	%d\n",cpu.ebp,cpu.ebp);
+		printf("esi:	0x%x	%d\n",cpu.esi,cpu.esi);
+		printf("edi:	0x%x	%d\n",cpu.edi,cpu.edi);
+		printf("esp:	0x%x	%d\n",cpu.esp,cpu.esp);
 	}
 	else if( strcmp(args,"w") == 0)
 	{
@@ -113,12 +113,9 @@ static int cmd_info(char* args)
 
 static int cmd_x(char *args)
 {
-	char* cnum=strtok(NULL," ");
+	//char* cnum=strtok(NULL," ");
 	//char* exp=strtok(NULL," ");
-	int len=strlen(cnum);
-	int num=0,i;
-	for( i=0;i<len;++i)
-		num=num*10+cnum[i];
+	//int num=atoi(cnum);
 	return 0;
 }
 
