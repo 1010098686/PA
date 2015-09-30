bed019670841f84bd4497f56f0b234f25f47736f
tracer-ics2015 <tracer@njuics.org>
2015-10-01 05:34:40 +0800

    >  compile NEMU 141220028 fangkang Linux fk 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  05:34:40 up  7:25,  8 users,  load average: 0.00, 0.01, 0.05 d2e032f9abb952942e2412a0a8ff44c1533105de

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -1,4 +1,5 @@
 #include"cpu/exec/template-start.h"
+#include<stdio.h>
 #define instr call
 
 void swaddr_write(swaddr_t addr , size_t len , uint32_t data);
@@ -8,7 +9,7 @@ static void do_execute()
 	cpu.esp=cpu.esp-4;
 	swaddr_write(cpu.esp,4,cpu.eip);
 	cpu.eip=cpu.eip+src;
-    op_src->val=cpu.eip;	
+    sprintf(op_src->str,"0x%x",cpu.eip);	
 	print_asm_template1();
 }
 
