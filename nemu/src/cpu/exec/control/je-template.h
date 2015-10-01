d194a14a11afe287dfe6a6504d6e14a56353a130
tracer-ics2015 <tracer@njuics.org>
2015-10-01 23:47:28 +0800

    >  compile NEMU 141220028 fangkang Linux fk 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  23:47:28 up  2:35,  8 users,  load average: 0.00, 0.01, 0.05 e5077c3460327359e07a09b2dd65e6d74f9fcc8

--- nemu/src/cpu/exec/control/je-template.h
+++ nemu/src/cpu/exec/control/je-template.h
@@ -3,30 +3,28 @@
 #define instr je
 static void do_execute()
 {
-	int asrc=0;
 	if(cpu.eflags.ZF==1)
 	{
 		
 	  if(DATA_BYTE==1)
 	   {
 		int8_t src=op_src->val;
-		asrc=src;
+		sprintf(op_src->str,"0x%x",cpu.eip+src+2);
 		cpu.eip+=src;
 	   }  
 	  else if(DATA_BYTE==2)
 	   {
 		int16_t src=op_src->val;
-		asrc=src;
+		sprintf(op_src->str,"0x%x",cpu.eip+src+2);
 		cpu.eip+=src;
 	   }
 	  else if(DATA_BYTE==4)
 	   {
 		int32_t src=op_src->val;
-		asrc=src;
+		sprintf(op_src->str,"0x%x",cpu.eip+src+2);
 		cpu.eip+=src;
 	   }
 	}
-	sprintf(op_src->str,"0x%x",cpu.eip+asrc+2);
 	print_asm_template1();
 }
 
