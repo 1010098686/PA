51e3e45ed240aaae771b3d12f5f218d9fc656f3e
tracer-ics2015 <tracer@njuics.org>
2015-10-02 03:17:30 +0800

    >  compile NEMU 141220028 fangkang Linux fk 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  03:17:30 up 27 min,  5 users,  load average: 0.00, 0.01, 0.03 d73c25b5a8d6f95cc4dfa848754d29fa99c44fe8

--- nemu/src/cpu/exec/logic/test-template.h
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -10,11 +10,14 @@ static void do_execute()
 		uint8_t dest=op_dest->val;
 		uint8_t result=src&dest;
 		if(result&0x80) cpu.eflags.SF=1;
+		else cpu.eflags.SF=0;
 		if(result==0) cpu.eflags.ZF=1;
+		else cpu.eflags.ZF=0;
 		result=result^(result>>4);
 		result=result^(result>>2);
 		result=result^(result>>1);
 		if(!(result&1)) cpu.eflags.PF=1;
+		else cpu.eflags.PF=0;
 	}
 	else if(DATA_BYTE==2)
 	{
@@ -22,12 +25,15 @@ static void do_execute()
 		uint16_t dest=op_dest->val;
 		uint16_t result=src&dest;
 		if(result&0x8000) cpu.eflags.SF=1;
+		else cpu.eflags.SF=0;
 		if(result==0) cpu.eflags.ZF=1;
+		else cpu.eflags.ZF=0;
 		uint8_t rel=result&0x00ff;
 		rel=rel^(rel>>4);
 		rel=rel^(rel>>2);
 		rel=rel^(rel>>1);
 		if(!(rel&1)) cpu.eflags.PF=1;
+		else cpu.eflags.PF=0;
 	}
 	else if(DATA_BYTE==4)
 	{
@@ -35,12 +41,15 @@ static void do_execute()
 		uint32_t dest=op_dest->val;
 		uint32_t result=src&dest;
 		if(result&0x80000000) cpu.eflags.SF=1;
+		else cpu.eflags.SF=0;
 		if(result==0) cpu.eflags.ZF=1;
+		else cpu.eflags.ZF=0;
 		uint8_t rel=result&0x000000ff;
 		rel=rel^(rel>>4);
 		rel=rel^(rel>>2);
 		rel=rel^(rel>>1);
 		if(!(rel&1)) cpu.eflags.PF=1;
+		else cpu.eflags.PF=0;
 	}
 	cpu.eflags.OF=0;
 	cpu.eflags.CF=0;
