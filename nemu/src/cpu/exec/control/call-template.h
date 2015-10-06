5d57f154b3ac33cda7dcd9cb000e178d23ef86aa
tracer-ics2015 <tracer@njuics.org>
2015-10-07 06:08:12 +0800

    >  compile NEMU 141220028 fangkang Linux fk 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  06:08:12 up  1:48,  5 users,  load average: 0.00, 0.01, 0.05 8cd57db478317886ce60e59cee424297897487f5

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -9,17 +9,17 @@ static void do_execute()
 	{
 	 int32_t src=op_src->val;
 	 cpu.esp=cpu.esp-4;
-	 swaddr_write(cpu.esp,4,cpu.eip);
-	 cpu.eip=cpu.eip+src+DATA_BYTE;
+	 swaddr_write(cpu.esp,4,cpu.eip+4);
+	 cpu.eip=cpu.eip+src;
 	}
 	else if(DATA_BYTE==2)
 	{
 		int16_t ip=cpu.eip&0x0000ffff;
 		cpu.esp=cpu.esp-2;
-		swaddr_write(cpu.esp,2,ip);
+		swaddr_write(cpu.esp,2,ip+2);
 		int16_t src=op_src->val;
 		cpu.eip+=src;
-		cpu.eip+=DATA_BYTE;
+		//cpu.eip+=DATA_BYTE;
 		cpu.eip=cpu.eip&0x0000ffff;
 	}
    print_asm_template1();
