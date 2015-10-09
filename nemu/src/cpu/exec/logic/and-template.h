13d06050f98e1e6e9058366fa65c7681e3e5d265
tracer-ics2015 <tracer@njuics.org>
2015-10-09 22:31:37 +0800

    >  compile NEMU 141220028 fangkang Linux fk 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  22:31:37 up  1:10,  5 users,  load average: 0.00, 0.01, 0.05 c8af5ba91cef4f92722b593bedaf5c6266f7ef

--- nemu/src/cpu/exec/logic/and-template.h
+++ nemu/src/cpu/exec/logic/and-template.h
@@ -3,7 +3,7 @@
 #define instr and
 
 static void do_execute () {
-	DATA_TYPE result = op_dest->val & op_src->val;
+	DATA_TYPE_S result = (DATA_TYPE_S)op_dest->val & (DATA_TYPE_S)op_src->val;
 	OPERAND_W(op_dest, result);
 
 	/* TODO: Update EFLAGS. */
