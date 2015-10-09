538f8b96e7f8d2e2b5b8c6e687711c3a70638823
tracer-ics2015 <tracer@njuics.org>
2015-10-09 21:32:52 +0800

    >  compile NEMU 141220028 fangkang Linux fk 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  21:32:52 up 12 min,  3 users,  load average: 0.08, 0.06, 0.06 e06d24d05f311be61bf78c7f512dcacaa77a9df3

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -65,12 +65,12 @@ make_group(group2_cl_v,
 /* 0xf6 */
 make_group(group3_b,
 	test_i2rm_b, inv, inv, inv, 
-	inv, inv, inv, inv)
+	inv, imul_rm2a_b, inv, inv)
 
 /* 0xf7 */
 make_group(group3_v,
 	test_i2rm_v, inv, inv, inv, 
-	inv, inv, inv, inv)
+	inv, imul_rm2a_v, inv, inv)
 
 /* 0xfe */
 make_group(group4,
