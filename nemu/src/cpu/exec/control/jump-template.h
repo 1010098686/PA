3ceca89bcdb77b7a514d32f503e157b4e4e94c15
tracer-ics2015 <tracer@njuics.org>
2015-10-09 20:14:14 +0800

    >  compile NEMU 141220028 fangkang Linux fk 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  20:14:14 up 40 min,  4 users,  load average: 0.08, 0.03, 0.05 a965590f2a4bcf53d8cae2c755969b2c3176266

--- nemu/src/cpu/exec/control/jump-template.h
+++ nemu/src/cpu/exec/control/jump-template.h
@@ -6,18 +6,18 @@ static void do_execute()
 {
 	if(DATA_BYTE==1)
 	{
-		uint8_t src=op_src->val;
+		int8_t src=op_src->val;
 		cpu.eip+=src;
 	}
 	else if(DATA_BYTE==2)
 	{
-		uint16_t src=op_src->val;
+		int16_t src=op_src->val;
 		cpu.eip+=src;
 		cpu.eip=cpu.eip&0x0000ffff;
 	}
 	else if(DATA_BYTE==4)
 	{
-		uint32_t src=op_src->val;
+		int32_t src=op_src->val;
 		cpu.eip+=src;
 	}
 	print_asm_template1();
