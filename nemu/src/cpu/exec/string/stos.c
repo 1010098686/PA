a093f2d56bd125ca5a08691d5f209be8ec685bf0
tracer-ics2015 <tracer@njuics.org>
2015-10-11 03:46:50 +0800

    >  compile NEMU 141220028 fangkang Linux fk 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  03:46:50 up  8:08,  5 users,  load average: 0.08, 0.03, 0.05 812d3efe6319e3829fe7414b57d38b17a238749b

--- nemu/src/cpu/exec/string/stos.c
+++ nemu/src/cpu/exec/string/stos.c
@@ -4,7 +4,7 @@ make_helper(stos)
 {
 	if(ops_decoded.opcode==0xaa) 
 	{
-		cpu.edi=cpu.gpr[0]._8[0];
+		swaddr_write(cpu.edi,1,cpu.gpr[0]._8[0]);
 		if(cpu.eflags.DF==0) ++cpu.edi;
 		else --cpu.edi;
 	}
@@ -12,13 +12,13 @@ make_helper(stos)
 	{
 		if(ops_decoded.is_data_size_16)
 		{
-			cpu.edi=cpu.gpr[0]._16;
+			swaddr_write(cpu.edi,2,cpu.gpr[0]._16);
 			if(cpu.eflags.DF==0) cpu.edi+=2;
 			else cpu.edi-=2;
 		}
 		else
 		{
-			cpu.edi=cpu.eax;
+			swaddr_write(cpu.edi,4,cpu.eax);
 			if(cpu.eflags.DF==0) cpu.edi+=4;
 			else cpu.edi-=4;
 		}
