e1ca66b2aede8d5afa52deb9a494c3e6400e7eb9
tracer-ics2015 <tracer@njuics.org>
2015-10-12 17:31:02 +0800

    >  compile NEMU 141220028 fangkang Linux fk 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  17:31:02 up  1:14,  6 users,  load average: 0.00, 0.03, 0.09 dcc4098290ef479438f434f6938253985cf0f605

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -3,9 +3,25 @@
 #define instr call
 
 void swaddr_write(swaddr_t addr , size_t len , uint32_t data);
+uint32_t swaddr_read(swaddr_t addr,size_t len);
 static void do_execute()
 {
-	if(DATA_BYTE==4)
+	if(ops_decoded.opcode==0xff)
+	{
+		if(DATA_BYTE==2)
+		{
+			cpu.esp-=2;
+			swaddr_write(cpu.esp,2,(cpu.eip+2)&0xffff);
+			cpu.eip=swaddr_read(op_src->val,4)&0xffff;
+		}
+		else if(DATA_BYTE==4)
+		{
+			cpu.esp-=4;
+			swaddr_write(cpu.esp,4,cpu.eip+2);
+			cpu.eip=swaddr_read(op_src->val,4);
+		}
+	}
+    else if(DATA_BYTE==4)
 	{
 	 cpu.esp-=4;
 	 MEM_W(cpu.esp,cpu.eip+4);
