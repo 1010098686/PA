b951a253857e7cc6aa79e027d1ad13c47c17ec9c
tracer-ics2015 <tracer@njuics.org>
2015-10-17 04:17:18 +0800

    >  compile NEMU 141220028 fangkang Linux fk 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  04:17:18 up  4:19, 11 users,  load average: 0.00, 0.06, 0.12 efb5a3fbb8f96bf97e69a49433608e23e577a34b

--- nemu/src/cpu/exec/control/ret-template.h
+++ nemu/src/cpu/exec/control/ret-template.h
@@ -8,18 +8,22 @@ static void do_execute()
 	{
 		uint16_t src=swaddr_read(cpu.esp,2);
 		cpu.esp+=2;
+		int len=decode_i_w(cpu.eip+1);
 		cpu.eip=cpu.eip&0xffff0000;
 		unsigned asrc=src;
 		cpu.eip+=asrc;
 		cpu.eip=cpu.eip&0x0000ffff;
 		cpu.eip+=imm;
+		cpu.eip-=len;
 	}
 	else
 	{
 		uint32_t src=swaddr_read(cpu.esp,4);
 		cpu.esp+=4;
+		int len=decode_i_l(cpu.eip+1);
 		cpu.eip=src;
 		cpu.eip+=imm;
+		cpu.eip-=len;
 	}
 	print_asm_template1();
 }
