d95d320e3c28b9c51da415b122707b0042dc2f8b
tracer-ics2015 <tracer@njuics.org>
2015-10-19 03:52:47 +0800

    >  compile kernel 141220028 fangkang Linux fk 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  03:52:47 up 16 min,  4 users,  load average: 0.08, 0.04, 0.05 73d16e9629e9f4b183e5e2267589b421bbf8dd59

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -2,6 +2,7 @@
 #include "memory.h"
 #include <string.h>
 #include <elf.h>
+#include <stdlib.h>
 
 #define ELF_OFFSET_IN_DISK 0
 
@@ -13,9 +14,9 @@ void ramdisk_read(uint8_t *, uint32_t, uint32_t);
 
 #define STACK_SIZE (1 << 20)
 
+void ramdisk_write(uint8_t *buf,uint32_t offset,uint32_t len);
 void create_video_mapping();
 uint32_t get_ucr3();
-void swaddr_write(swaddr_t addr,size_t len,uint32_t data);
 uint32_t loader() {
 	Elf32_Ehdr *elf;
 	Elf32_Phdr *ph = NULL;
@@ -40,23 +41,24 @@ uint32_t loader() {
 	int i;
 	for(i=0;i<elf->e_phnum;++i) {
 		/* Scan the program header table, load each segment into memory */
-		ph=elf+elf->e_phoff+i*elf->phentsize;
+		ph=(Elf32_Phdr*)(elf+elf->e_phoff+i*elf->e_phentsize);
 		if(ph->p_type == PT_LOAD) {
 
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			uint8_t content=(uint8_t*)malloc(sizeof(uint8_t)*ph->p_filesz); 
-			ramdisk_read(content,ph->p_offset,ph->filesz);
+			uint8_t* content=(uint8_t*)malloc(sizeof(uint8_t)*ph->p_filesz); 
+			ramdisk_read(content,ph->p_offset,ph->p_filesz);
 			int j;
-			for(j=0;j<ph->p_filesz;++j)
-				swaddr_write(ph->vaddr+i,1,content[i]);
-			 
+			ramdisk_write(content,ph->p_vaddr,ph->p_filesz);
+			free(content); 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			for(j=ph->vaddr+ph->p_filesz;j<ph->p_vaddr+ph->p_memsz;++j)
-				swaddr_write(j,1,0);
+			uint8_t* zero=(uint8_t*)malloc(sizeof(uint8_t)*(ph->p_memsz-ph->p_filesz));
+			for(j=0;j<ph->p_memsz-ph->p_filesz;++j) zero[i]=0;
+			ramdisk_write(zero,ph->p_vaddr,ph->p_memsz-ph->p_filesz);
+			free(zero);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
