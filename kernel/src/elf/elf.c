836784fb2b057641b6bb128328296f692cd3f11c
tracer-ics2015 <tracer@njuics.org>
2015-10-21 22:11:02 +0800

    >  compile kernel 141220028 fangkang Linux fk 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  22:11:02 up  1:03,  4 users,  load average: 0.02, 0.07, 0.20 ffe07e1d4c5a1ce64489648aac84160d24d64740

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -43,7 +43,8 @@ uint32_t loader() {
 		/* Scan the program header table, load each segment into memory */
 		//int size=sizeof(Elf32_Ehdr);
 		//ph=(void*)(elf+(elf->e_phoff)/size+(i*elf->e_phentsize)/size);
-		ph=(void*)(buf+elf->e_phoff+i*elf->e_phentsize);
+		//ph=(void*)(buf+elf->e_phoff+i*elf->e_phentsize);
+        ph=(void*)(elf+elf->e_phoff+i*elf->e_phentsize);
 		if(ph->p_type == PT_LOAD) {
 
 			/* TODO: read the content of the segment from the ELF file 
