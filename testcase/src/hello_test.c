c37195d4114f61b26e9a09e1ed50281013712065
tracer-ics2015 <tracer@njuics.org>
2015-10-25 14:04:44 +0800

    >  run 141220028 fangkang Linux fangkang 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u5 (2015-10-09) i686 GNU/Linux  14:04:44 up  3:00,  2 users,  load average: 0.10, 0.10, 0.12 ea57d77efafc6d65a6b9f61c106a3fd93cc7d84

---
+++ testcase/src/hello_test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "trap.h"
+
+char buf[128];
+
+/* This function is defined only to make the newlibc linkable.
+ * Without it, errors will be reported during linking.
+ * But the execution flow should not reach here.
+ */
+void *sbrk(int incr) {
+	nemu_assert(0);
+	return NULL;
+}
+
+int main() {
+	sprintf(buf, "%s", "a\n");
+	nemu_assert(strcmp(buf, "a\n") == 0);
+
+	
+
+	HIT_GOOD_TRAP;
+	return 0;
+}
