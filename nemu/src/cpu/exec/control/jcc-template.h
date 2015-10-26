c89c344f0995e357c678e615717d7447395bd072
tracer-ics2015 <tracer@njuics.org>
2015-10-26 20:54:27 +0800

    >  compile NEMU 141220028 fangkang Linux fangkang 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u5 (2015-10-09) i686 GNU/Linux  20:54:27 up  2:37,  2 users,  load average: 0.30, 0.24, 0.21 480ad0ee22a20a2e9c76036f729087535dce944

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -7,38 +7,38 @@ static void do_execute()
    bool flag=false;
    switch(ops_decoded.opcode)
    {
-     case 0x77:
+     case 0x77:flag=(cpu.eflags.CF==0 && cpu.eflags.ZF==0);break;
      case 0x87:flag=(cpu.eflags.CF==0 && cpu.eflags.ZF==0);break;
-     case 0x73:
+     case 0x73:flag=(cpu.eflags.CF==0);break;
      case 0x83:flag=(cpu.eflags.CF==0);break;
      case 0xe3:flag=(cpu.ecx==0);break;
-     case 0x74:
+     case 0x74:flag=(cpu.eflags.ZF==1);break;
      case 0x84:flag=(cpu.eflags.ZF==1);break;
-     case 0x7f:
+     case 0x7f:flag=(cpu.eflags.ZF==0 && cpu.eflags.SF==cpu.eflags.OF);break;
      case 0x8f:flag=(cpu.eflags.ZF==0 && cpu.eflags.SF==cpu.eflags.OF);break;
-     case 0x7d:
+     case 0x7d:flag=(cpu.eflags.SF==cpu.eflags.OF);break;
      case 0x8d:flag=(cpu.eflags.SF==cpu.eflags.OF);break;
-     case 0x7c:
+     case 0x7c:flag=(cpu.eflags.SF!=cpu.eflags.OF);break;
      case 0x8c:flag=(cpu.eflags.SF!=cpu.eflags.OF);break;
-     case 0x7e:
+     case 0x7e:flag=(cpu.eflags.ZF==1 || cpu.eflags.SF!=cpu.eflags.OF);break;
      case 0x8e:flag=(cpu.eflags.ZF==1 || cpu.eflags.SF!=cpu.eflags.OF);break;
-     case 0x76:
+     case 0x76:flag=(cpu.eflags.CF==1 || cpu.eflags.ZF==1); break;
      case 0x86:flag=(cpu.eflags.CF==1 || cpu.eflags.ZF==1); break;
-     case 0x72:
+     case 0x72:flag=(cpu.eflags.CF==1);break;
      case 0x82:flag=(cpu.eflags.CF==1);break;
-     case 0x75:
+     case 0x75:flag=(cpu.eflags.ZF==0);break;
      case 0x85:flag=(cpu.eflags.ZF==0);break;
-     case 0x71:
+     case 0x71:flag=(cpu.eflags.OF==0);break;
      case 0x81:flag=(cpu.eflags.OF==0);break;
-     case 0x7b:
+     case 0x7b:flag=(cpu.eflags.PF==0);break;
      case 0x8b:flag=(cpu.eflags.PF==0);break;
-     case 0x79:
+     case 0x79:flag=(cpu.eflags.SF==0);break;
      case 0x89:flag=(cpu.eflags.SF==0);break;
-     case 0x70:
+     case 0x70:flag=(cpu.eflags.OF==1);break;
      case 0x80:flag=(cpu.eflags.OF==1);break;
-     case 0x7a:
+     case 0x7a:flag=(cpu.eflags.PF==1);break;
      case 0x8a:flag=(cpu.eflags.PF==1);break;
-     case 0x78:
+     case 0x78:flag=(cpu.eflags.SF==1);break;
      case 0x88:flag=(cpu.eflags.SF==1);break;
      default :flag=false;
      }
