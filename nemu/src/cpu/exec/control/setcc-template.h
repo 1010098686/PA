ebb1da44ba5a622b3ce68ad2450f6e5072e62149
tracer-ics2015 <tracer@njuics.org>
2015-10-26 21:37:07 +0800

    >  compile NEMU 141220028 fangkang Linux fangkang 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u5 (2015-10-09) i686 GNU/Linux  21:37:07 up  3:20,  2 users,  load average: 0.11, 0.05, 0.08 4a0b76eae035d56df0e7232ff794acfaf7812b4c

--- nemu/src/cpu/exec/control/setcc-template.h
+++ nemu/src/cpu/exec/control/setcc-template.h
@@ -7,22 +7,22 @@ static void do_execute()
    bool flag=false;
    switch(ops_decoded.opcode)
    {
-     case 0x97:flag=(cpu.eflags.CF==0 && cpu.eflags.ZF==0);break;
-     case 0x93:flag=(cpu.eflags.CF==0);break;
-     case 0x92:flag=(cpu.eflags.CF==1);break;
-     case 0x96:flag=(cpu.eflags.CF==1 || cpu.eflags.ZF==1);break;
-     case 0x94:flag=(cpu.eflags.ZF==1);break;
-     case 0x9f:flag=(cpu.eflags.ZF==0 && cpu.eflags.SF==cpu.eflags.OF);break;
-     case 0x9d:flag=(cpu.eflags.SF==cpu.eflags.OF);break;
-     case 0x9c:flag=(cpu.eflags.SF!=cpu.eflags.OF);break;
-     case 0x9e:flag=(cpu.eflags.ZF==1 || cpu.eflags.SF!=cpu.eflags.OF);break;
-     case 0x95:flag=(cpu.eflags.ZF==0);break;
-     case 0x91:flag=(cpu.eflags.OF==0);break;
-     case 0x9b:flag=(cpu.eflags.PF==0);break;
-     case 0x99:flag=(cpu.eflags.SF==0);break;
-     case 0x90:flag=(cpu.eflags.OF==1);break;
-     case 0x9a:flag=(cpu.eflags.PF==1);break;
-     case 0x98:flag=(cpu.eflags.SF==1);break;
+     case 0x197:flag=(cpu.eflags.CF==0 && cpu.eflags.ZF==0);break;
+     case 0x193:flag=(cpu.eflags.CF==0);break;
+     case 0x192:flag=(cpu.eflags.CF==1);break;
+     case 0x196:flag=(cpu.eflags.CF==1 || cpu.eflags.ZF==1);break;
+     case 0x194:flag=(cpu.eflags.ZF==1);break;
+     case 0x19f:flag=(cpu.eflags.ZF==0 && cpu.eflags.SF==cpu.eflags.OF);break;
+     case 0x19d:flag=(cpu.eflags.SF==cpu.eflags.OF);break;
+     case 0x19c:flag=(cpu.eflags.SF!=cpu.eflags.OF);break;
+     case 0x19e:flag=(cpu.eflags.ZF==1 || cpu.eflags.SF!=cpu.eflags.OF);break;
+     case 0x195:flag=(cpu.eflags.ZF==0);break;
+     case 0x191:flag=(cpu.eflags.OF==0);break;
+     case 0x19b:flag=(cpu.eflags.PF==0);break;
+     case 0x199:flag=(cpu.eflags.SF==0);break;
+     case 0x190:flag=(cpu.eflags.OF==1);break;
+     case 0x19a:flag=(cpu.eflags.PF==1);break;
+     case 0x198:flag=(cpu.eflags.SF==1);break;
      default:flag=false;
      }
      if(flag) OPERAND_W(op_src,1);
--- nemu/src/cpu/exec/data-mov/cmovcc-template.h
+++ nemu/src/cpu/exec/data-mov/cmovcc-template.h
@@ -7,22 +7,22 @@ static void do_execute()
   bool flag=false;
   switch(ops_decoded.opcode)
   {
-    case 0x47:flag=(cpu.eflags.CF==0 && cpu.eflags.ZF==0);break;
-    case 0x43:flag=(cpu.eflags.CF==0);break;
-    case 0x42:flag=(cpu.eflags.CF==1);break;
-    case 0x46:flag=(cpu.eflags.CF==1 || cpu.eflags.ZF==1);break;
-    case 0x44:flag=(cpu.eflags.ZF==1);break;
-    case 0x4f:flag=(cpu.eflags.ZF==0 && cpu.eflags.SF==cpu.eflags.OF);break;
-    case 0x4d:flag=(cpu.eflags.SF==cpu.eflags.OF);break;
-    case 0x4c:flag=(cpu.eflags.SF!=cpu.eflags.OF);break;
-    case 0x4e:flag=(cpu.eflags.ZF==1 || cpu.eflags.SF!=cpu.eflags.OF);break;
-    case 0x45:flag=(cpu.eflags.ZF==0);break;
-    case 0x41:flag=(cpu.eflags.OF==0);break;
-    case 0x4b:flag=(cpu.eflags.PF==0);break;
-    case 0x49:flag=(cpu.eflags.SF==0);break;
-    case 0x40:flag=(cpu.eflags.OF==1);break;
-    case 0x4a:flag=(cpu.eflags.PF==1);break;
-    case 0x48:flag=(cpu.eflags.SF==1);break;
+    case 0x147:flag=(cpu.eflags.CF==0 && cpu.eflags.ZF==0);break;
+    case 0x143:flag=(cpu.eflags.CF==0);break;
+    case 0x142:flag=(cpu.eflags.CF==1);break;
+    case 0x146:flag=(cpu.eflags.CF==1 || cpu.eflags.ZF==1);break;
+    case 0x144:flag=(cpu.eflags.ZF==1);break;
+    case 0x14f:flag=(cpu.eflags.ZF==0 && cpu.eflags.SF==cpu.eflags.OF);break;
+    case 0x14d:flag=(cpu.eflags.SF==cpu.eflags.OF);break;
+    case 0x14c:flag=(cpu.eflags.SF!=cpu.eflags.OF);break;
+    case 0x14e:flag=(cpu.eflags.ZF==1 || cpu.eflags.SF!=cpu.eflags.OF);break;
+    case 0x145:flag=(cpu.eflags.ZF==0);break;
+    case 0x141:flag=(cpu.eflags.OF==0);break;
+    case 0x14b:flag=(cpu.eflags.PF==0);break;
+    case 0x149:flag=(cpu.eflags.SF==0);break;
+    case 0x140:flag=(cpu.eflags.OF==1);break;
+    case 0x14a:flag=(cpu.eflags.PF==1);break;
+    case 0x148:flag=(cpu.eflags.SF==1);break;
     default:flag=false;
     }
     if(flag) OPERAND_W(op_dest,op_src->val);
