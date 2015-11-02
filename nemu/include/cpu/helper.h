1ea4c3af38c5c3f27eb478c3b3fd51d69778190c
tracer-ics2015 <tracer@njuics.org>
2015-11-02 16:37:58 +0800

    >  compile NEMU 141220028 fangkang Linux fangkang 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u5 (2015-10-09) i686 GNU/Linux  16:37:58 up 7 min,  3 users,  load average: 0.20, 0.68, 0.47 9bae81791cda883cd019e922268aded3b7571fb7

--- nemu/include/cpu/helper.h
+++ nemu/include/cpu/helper.h
@@ -7,12 +7,12 @@
 /* All function defined with 'make_helper' return the length of the operation. */
 #define make_helper(name) int name(swaddr_t eip)
 
- static inline uint32_t instr_fetch(swaddr_t addr, size_t len) {
+static  inline uint32_t instr_fetch(swaddr_t addr, size_t len) {
 	return swaddr_read(addr, len);
 }
 
 /* Instruction Decode and EXecute */
-static inline int idex(swaddr_t eip, int (*decode)(swaddr_t), void (*execute) (void)) {
+static  inline int idex(swaddr_t eip, int (*decode)(swaddr_t), void (*execute) (void)) {
 	/* eip is pointing to the opcode */
 	int len = decode(eip + 1);
 	execute();
