fefc65f4bc19938809bd0792622db7abea940005
tracer-ics2015 <tracer@njuics.org>
2015-11-16 19:00:24 +0800

    >  compile NEMU 141220028 fangkang Linux fangkang 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u5 (2015-10-09) i686 GNU/Linux  19:00:24 up 17 min,  2 users,  load average: 0.26, 0.36, 0.34 f02f81e88d83f5f322b83c2db844f922192d3e5

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -7,7 +7,7 @@ void dram_write(hwaddr_t, size_t, uint32_t);
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-        int num;
+        /*int num;
         if(hit(addr,&num))
         {
           int offset=addr&0x00003f;
@@ -36,9 +36,9 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
           {
             uint8_t temp=dram_read(newaddr+i,1)&0xff;
             cpu.cache.cache_group[cache_index(newaddr)].cache_block[_index].data[i]=temp;
-          }
+          }*/
 	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
-	}
+	//}
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
