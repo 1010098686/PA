817d84eda8eaf92565be9792cce4f63cdf9bc49a
tracer-ics2015 <tracer@njuics.org>
2015-11-16 16:30:46 +0800

    >  compile NEMU 141220028 fangkang Linux fangkang 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u5 (2015-10-09) i686 GNU/Linux  16:30:46 up 9 min,  2 users,  load average: 0.52, 0.60, 0.50 92e99401cd3ac1897d1b2eaef16d930fd148bdcf

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -6,7 +6,7 @@ typedef struct
 {
    unsigned int valid:1;
    unsigned int tag:11;
-   int8_t block[64];
+   uint8_t data[64];
 } Cache_block;
 
 typedef struct 
@@ -20,5 +20,24 @@ typedef struct
 } Cache;
 
  extern Cache cache;
+ 
+ #define index(addr) ((addr)&0x001fc0)>>6
+ #define tag(addr) ((addr)&0xffe000)>>13
+ 
+ 
+ static inline bool hit(hwaddr_t addr,int*num)
+ {
+    int _index=index(addr);
+    int i;
+    for(i=0;i<8;++i) 
+    {
+      if(cache.cache_group[_index].cache_block[i].tag==tag(addr) && cache.cache_group[_index].cache_block[i].valid==1) 
+      {
+        *num=i;
+        return true;
+      }
+    }
+    return false;
+}
 
 #endif
--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,12 +1,29 @@
 #include "common.h"
-
+#include<stdlib.h>
+#include<time.h>
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
+        /*int num;
+        if(hit(addr,&num)
+        {
+          int offset=addr&0xffff3f+len;
+          int i;
+          uint32_t result=0;
+          for(i=offset-1;i>=addr&0xffff3f;--i)
+          {
+            result=result<<8+cache.cache_group[index(addr)].cache_block[num].data[i];
+          }
+          return result;
+        }
+        else
+        {*/
+          
 	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+	//}
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
