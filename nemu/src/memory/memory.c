562f93c81952b49baeb50edbaead745ba8b65756
tracer-ics2015 <tracer@njuics.org>
2015-11-16 17:08:46 +0800

    >  compile NEMU 141220028 fangkang Linux fangkang 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u5 (2015-10-09) i686 GNU/Linux  17:08:46 up 47 min,  2 users,  load average: 1.01, 1.02, 0.66 513a6333c085eaf0252ad32229d8ba80c35c01b3

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -33,7 +33,7 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
           cache.cache_group[cache_index(newaddr)].cache_block[_index].tag=tag(newaddr);
           for(i=0;i<64;++i)
           {
-            uint8_t temp=dram_read(newaddr+i,1)&0xf;
+            uint8_t temp=dram_read(newaddr+i,1)&0xff;
             cache.cache_group[cache_index(newaddr)].cache_block[_index].data[i]=temp;
           }
 	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
