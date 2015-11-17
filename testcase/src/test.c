4ed9a5d075f88af32d2a9c0e878bfaef99f1140e
tracer-ics2015 <tracer@njuics.org>
2015-11-17 18:41:09 +0800

    >  run 141220028 fangkang Linux fangkang 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u5 (2015-10-09) i686 GNU/Linux  18:41:09 up 12 min,  2 users,  load average: 0.51, 0.61, 0.56 6514f39bce7c5f41fdd84621fa2ef1aebc7c8ef4

--- testcase/src/test.c
+++ testcase/src/test.c
@@ -1,12 +1,10 @@
 #include"trap.h"
-unsigned char  ans[]={0,1,2,3,4,5,6,7,8,9};
+int ans[]={0,-1,-2,-3,-4,-5,-6,-7,-8,-9};
 
 int main()
 {
-  unsigned char i=0;
-  nemu_assert(i==ans[0]);
-  for(i=0;i<10;++i) ans[i]=9-i;
-  for(i=0;i<10;++i) nemu_assert(ans[i]==9-i);
+  int i;
+  for(i=0;i<10;++i) nemu_assert(ans[i]==-i);
   HIT_GOOD_TRAP;
   return 0;
 }
