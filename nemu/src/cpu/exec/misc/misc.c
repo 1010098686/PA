0dc530be5df44ee42e014333c37518d8a144e47a
tracer-ics2015 <tracer@njuics.org>
2015-11-24 09:07:36 +0800

    >  compile NEMU 141220028 fangkang Linux fangkang 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u5 (2015-10-09) i686 GNU/Linux  09:07:36 up 26 min,  2 users,  load average: 0.30, 0.21, 0.28 4073282895d51e03a58ea49c44b3065ffde53be5

--- nemu/src/cpu/exec/misc/misc.c
+++ nemu/src/cpu/exec/misc/misc.c
@@ -86,7 +86,7 @@ make_helper(cld)
 }
 make_helper(lgdt)
 {
-   uint32_t addr=swaddr_read(cpu.eip+2,4,0);
+   uint32_t addr=swaddr_read(cpu.eip+1,4,0);
    uint32_t limit=swaddr_read(addr,2,2);
    uint32_t base_addr=swaddr_read(addr+2,4,2);
    cpu.GDTR.limit=limit;
