54f5bc239a4402bd785cf1ff8ed7af6d1ae5fa97
tracer-ics2015 <tracer@njuics.org>
2015-11-26 21:32:37 +0800

    >  compile NEMU 141220028 fangkang Linux fangkang 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u5 (2015-10-09) i686 GNU/Linux  21:32:37 up  1:59,  2 users,  load average: 0.45, 0.22, 0.18 87b42b38f96a89527eb00a9e79b1b6bbed604d51

--- nemu/src/cpu/exec/data-mov/crmov-template.h
+++ nemu/src/cpu/exec/data-mov/crmov-template.h
@@ -1,13 +1,29 @@
 #include "cpu/exec/template-start.h"
-
+#include "cpu/decode/modrm.h"
 #define instr crmov
 
 static void do_execute()
 {
-  if(ops_decoded.opcode==0x120) 
-   OPERAND_W(op_dest,cpu.CR0.val);
+  ModR_M m;
+  m.val=swaddr_read(cpu.eip+2,1,0);
+  if(ops_decoded.opcode==0x120)
+  { 
+   switch (m.reg)
+   {
+      case 0:OPERAND_W(op_dest,cpu.CR0.val);break;
+      case 2:OPERAND_W(op_dest,cpu.CR3.val);break;
+      default:panic("error");
+   }
+  }
   else if(ops_decoded.opcode==0x122)
-    cpu.CR0.val=op_src->val;
+   {
+     switch(m.reg)
+     {
+       case 0:cpu.CR0.val=op_src->val;break;
+       case 2:cpu.CR3.val=op_src->val;break;
+       default:panic("error");
+      }
+   }
   else panic("error\n");
   print_asm_template2();
 }
