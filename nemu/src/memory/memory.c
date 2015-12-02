cdff174894b5fc508a7a8de57bca5fbb9e906c0a
tracer-ics2015 <tracer@njuics.org>
2015-12-02 20:16:44 +0800

    >  compile NEMU 141220028 fangkang Linux ubuntu 4.2.0-16-generic #19-Ubuntu SMP Thu Oct 8 14:46:51 UTC 2015 i686 i686 i686 GNU/Linux  20:16:44 up 43 min,  3 users,  load average: 0.26, 0.15, 0.16 345e8cedb424d562df4109a3d21a65d69c8b2f2c

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -31,12 +31,12 @@ hwaddr_t page_translate(lnaddr_t addr,int* flag)
 {
    PDE pdir;
    pdir.val=hwaddr_read((cpu.CR3.page_directory_base<<12)+((addr>>22)&0x000003ff)*4,4);
-   printf("0x%x\n",pdir.val);
-   printf("0x%x\n",pdir.present);
+//   printf("0x%x\n",pdir.val);
+//   printf("0x%x\n",pdir.present);
    if(pdir.present==0) { *flag=-1; return 0;}
    PTE ptable;
    ptable.val=hwaddr_read((pdir.page_frame<<12)+((addr>>12)&0x000003ff)*4,4);
-   printf("0x%x\n",ptable.val);
+//   printf("0x%x\n",ptable.val);
    if(ptable.present==0) {*flag=0; return 0;}
    *flag=1;
    return (((uint32_t)ptable.page_frame)<<12)+(addr&0x00000fff);
