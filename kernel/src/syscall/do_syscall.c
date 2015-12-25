3f720f71a5c846b0f212543279ea5200dabcf910
tracer-ics2015 <tracer@njuics.org>
2015-12-26 02:01:46 +0800

    >  compile kernel 141220028 fangkang Linux fk 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  02:01:46 up 3 min,  2 users,  load average: 0.09, 0.03, 0.02 26a572bfb6df185cc6e984895958d68b86ca66fe

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -27,9 +27,8 @@ void do_syscall(TrapFrame *tf) {
 			break;
 
 		case SYS_brk: sys_brk(tf); break;
-    //case SYS_write:asm volatile(".byte 0xd6" : : "a"(2),"c"(tf->ecx),"d"(tf->edx)); tf->eax = strlen((char*)tf->ecx); break;
 		/* TODO: Add more system calls. */
-    case SYS_write:
+        case SYS_write:
 		      {
 						int i;
 						for(i=0;i<tf->edx;++i) serial_printc(*((char*)tf->ecx+i));
