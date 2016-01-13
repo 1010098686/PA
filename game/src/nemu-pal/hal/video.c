4e2eb2786b919206d95d650b208f6cb55ab6538f
tracer-ics2015 <tracer@njuics.org>
2016-01-13 20:39:51 +0800

    >  compile game 141220028 fangkang Linux fk 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  20:39:51 up  3:35,  3 users,  load average: 0.06, 0.07, 0.12 401b9fc6276f48625d9019b3a5f7b6f4c344297

--- game/src/nemu-pal/hal/video.c
+++ game/src/nemu-pal/hal/video.c
@@ -18,11 +18,8 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *scrrect,
 	 * (``src'' or ``dst'') is copied. The final blit rectangle 
 	 * is saved in ``dstrect'' after all clipping is performed
 	 * (``srcrect'' is not modified).
-	 */
-         if(scrrect==NULL)
-         {
-		 }           
-	//assert(0);
+	 */ 
+	assert(0);
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
