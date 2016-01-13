a74243ff4c1aca35a74cdeb937221035ee4180cc
tracer-ics2015 <tracer@njuics.org>
2016-01-13 20:30:22 +0800

    >  compile kernel 141220028 fangkang Linux fk 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  20:30:22 up  3:26,  3 users,  load average: 0.01, 0.18, 0.17 c39bf633f3882f76e75b65adfbba369755b4bd59

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -75,7 +75,7 @@ void init_cond() {
 
 #if defined(IA32_PAGE) && defined(HAS_DEVICE)
 	/* Write some test data to the video memory. */
-	video_mapping_write_test();
+//	video_mapping_write_test();
 #endif
 
 	/* Load the program. */
@@ -85,10 +85,10 @@ void init_cond() {
 	/* Read data in the video memory to check whether 
 	 * the test data is written sucessfully.
 	 */
-	video_mapping_read_test();
+//	video_mapping_read_test();
 
 	/* Clear the test data we just written in the video memory. */
-	video_mapping_clear();
+//	video_mapping_clear();
 #endif
 
 #ifdef IA32_PAGE
