79e7bdb96491fd1fd107acd2f72b960b17437004
tracer-ics2015 <tracer@njuics.org>
2016-01-17 12:28:55 +0800

    >  compile NEMU 141220028 fangkang Linux fangkang 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u5 (2015-10-09) i686 GNU/Linux  12:28:55 up 47 min,  3 users,  load average: 1.09, 0.57, 0.33 350cc09479d53c7d10b4d7146f91d96fa27ff8da

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -31,8 +31,8 @@ make_helper(rep) {
 			/* TODO: Jump out of the while loop if necessary. */
 			if(ops_decoded.opcode==0xa6 || ops_decoded.opcode==0xa7 || ops_decoded.opcode==0xae || ops_decoded.opcode==0xaf) //cmpsb cmpsw scasb scasw
 			{
-			  if(opcode==0xf3 && cpu.eflags.ZF==0) break; //repnz repne
-			  if(opcode==0xf2 && cpu.eflags.ZF==1) break; //rep repe repz
+			  if(opcode==0xf2 && cpu.eflags.ZF==0) break; //repnz repne
+			  if(opcode==0xf3 && cpu.eflags.ZF==1) break; //rep repe repz
 			}
 		}
 		len = 1;
