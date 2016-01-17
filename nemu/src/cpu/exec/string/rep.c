7cf4e9564aa29d7bfb57c2daf22cb38d68da62f1
tracer-ics2015 <tracer@njuics.org>
2016-01-17 12:26:02 +0800

    >  compile NEMU 141220028 fangkang Linux fangkang 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u5 (2015-10-09) i686 GNU/Linux  12:26:02 up 44 min,  3 users,  load average: 0.39, 0.16, 0.18 af9f49dd5d503ba4526877d7ed27c64c45be1d13

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -10,7 +10,7 @@ make_helper(rep) {
 	if(instr_fetch(eip + 1, 1) == 0xc3) {
 		/* repz ret */
 		exec(eip + 1);
-		len = 0;
+		len = -1;
 	}
 	else {
 		while(cpu.ecx) {
@@ -29,10 +29,10 @@ make_helper(rep) {
 				);
 
 			/* TODO: Jump out of the while loop if necessary. */
-			if(ops_decoded.opcode==0xa6 || ops_decoded.opcode==0xa7 || ops_decoded.opcode==0xae || ops_decoded.opcode==0xaf)
+			if(ops_decoded.opcode==0xa6 || ops_decoded.opcode==0xa7 || ops_decoded.opcode==0xae || ops_decoded.opcode==0xaf) //cmpsb cmpsw scasb scasw
 			{
-			  if(opcode==0xf3 && cpu.eflags.ZF==0) break;
-			  if(opcode==0xf2 && cpu.eflags.ZF==1) break;
+			  if(opcode==0xf3 && cpu.eflags.ZF==0) break; //repnz repne
+			  if(opcode==0xf2 && cpu.eflags.ZF==1) break; //rep repe repz
 			}
 		}
 		len = 1;
